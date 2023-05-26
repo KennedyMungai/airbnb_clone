b3e4471732c87d5d047ffcdd169bb85fbd8b714e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 13:11:03 +0300

    Added a child div with some padding and a border

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 type Props = {}
 
 const NavBar = (props: Props) => {
-	return <div className='fixed w-full bg-white z-10 shadow-sm'>NavBar</div>
+	return (
+		<div className='fixed w-full bg-white z-10 shadow-sm'>
+			<div className='py-4 border-b-[1px]'></div>
+		</div>
+	)
 }
 
 export default NavBar
