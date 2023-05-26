97a8e808e15fdc6aa99dc76a763691feedabe93b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 13:49:43 +0300

    Added a bunch of classes to the parent div

--- src/components/navbar/Search.tsx
+++ src/components/navbar/Search.tsx
@@ -1,7 +1,11 @@
 type Props = {}
 
 const Search = (props: Props) => {
-	return <div>Search</div>
+	return (
+		<div className='border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer'>
+			Search
+		</div>
+	)
 }
 
 export default Search
