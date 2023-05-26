c60d490982900e1960f3932cd34ecacd4ea92064
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 13:35:27 +0300

    Added the Logo image to the component

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -8,7 +8,15 @@ type Props = {}
 const Logo = (props: Props) => {
 	const router = useRouter()
 
-	return <div>Logo</div>
+	return (
+		<Image
+			alt='Logo'
+			className='hidden md:block cursor-pointer'
+			height={100}
+			width={100}
+			src={'/images/logo.png'}
+		/>
+	)
 }
 
 export default Logo
