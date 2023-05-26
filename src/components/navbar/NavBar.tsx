b8b572d8898ce2c632651dc84522de3a8a9f43de
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 13:17:35 +0300

    Added some sample text

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = (props: Props) => {
 	return (
 		<div className='fixed w-full bg-white z-10 shadow-sm'>
 			<div className='py-4 border-b-[1px]'>
-				<Container></Container>
+				<Container>Climbing Up The Walls</Container>
 			</div>
 		</div>
 	)
