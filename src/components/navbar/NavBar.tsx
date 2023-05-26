bbd17f7b4dc8973e027b7e4f329aa127fde758e4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 13:11:55 +0300

    Added theyet to be created Container component

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -5,7 +5,9 @@ type Props = {}
 const NavBar = (props: Props) => {
 	return (
 		<div className='fixed w-full bg-white z-10 shadow-sm'>
-			<div className='py-4 border-b-[1px]'></div>
+			<div className='py-4 border-b-[1px]'>
+				<Container></Container>
+			</div>
 		</div>
 	)
 }
