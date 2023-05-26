ce8aaf9fcb4a1a04eb7cce5dc564b012d0c119a9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 13:53:24 +0300

    Added two purposefully styled divs

--- src/components/navbar/Search.tsx
+++ src/components/navbar/Search.tsx
@@ -4,7 +4,12 @@ type Props = {}
 const Search = (props: Props) => {
 	return (
 		<div className='border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer'>
-			Search
+			<div className='flex flex-row items-center justify-between'>
+				<div className='text-sm font-semibold px-6'>AnyWhere</div>
+				<div className='hidden sm:block text-sm font-semibold px-6 flex-1 text-center border-x-[1px]'>
+					Any Week
+				</div>
+			</div>
 		</div>
 	)
 }
