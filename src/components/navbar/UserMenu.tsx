e114a105c45eab722fae186885b8a63f6db767b4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-29 12:36:34 +0300

    Added the avatar component

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { AiOutlineMenu } from 'react-icons/ai'
+import Avatar from '../Avatar'
 
 type Props = {}
 
@@ -18,6 +19,9 @@ const UserMenu = (props: Props) => {
 					onClick={() => {}}
 				>
 					<AiOutlineMenu />
+					<div className='hidden md:block'>
+						<Avatar />
+					</div>
 				</div>
 			</div>
 		</div>
