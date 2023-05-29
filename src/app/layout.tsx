a76f12a2eb4d307ba42c16b7595beafabec632a7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-29 16:20:16 +0300

    Added a placeholder title for the modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={font.className}>
-				<Modal isOpen />
+				<Modal isOpen title='I Am A Good Modal' />
 				<NavBar />
 				{children}
 			</body>
