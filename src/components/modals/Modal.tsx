9aabb31e4c71b92cd2ca3688513e7e3318cbb8c6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-29 15:13:45 +0300

    Wrote the types for the Props of the component

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { ReactElement } from 'react'
 
-type Props = {}
+type Props = {
+	isOpen?: boolean
+	onClose: () => void
+	onSubmit: () => void
+	title?: string
+	body?: ReactElement
+	footer?: ReactElement
+	actionLabel: string
+	disabled?: boolean
+	secondaryAction?: () => void
+	secondaryLabel?: string
+}
 
 const Modal = (props: Props) => {
 	return <div>Modal</div>
