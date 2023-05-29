46615957e9959b2ecdb73cd78967c9c879f57d41
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-29 15:02:16 +0300

    Rendered out the label passed onto the MenuItem

--- src/components/navbar/MenuItem.tsx
+++ src/components/navbar/MenuItem.tsx
@@ -9,7 +9,9 @@ const MenuItem = ({ onClick, label }: Props) => {
 		<div
 			className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
 			onClick={onClick}
-		></div>
+		>
+			{label}
+		</div>
 	)
 }
 
