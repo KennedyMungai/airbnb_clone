4270e3e4496711d02d4c408fbe7035c1a54b3a3a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-29 12:34:55 +0300

    Added a menu hamburger

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -1,8 +1,10 @@
 'use client'
+import { AiOutlineMenu } from 'react-icons/ai'
+
 type Props = {}
 
 const UserMenu = (props: Props) => {
-    return (
+	return (
 		<div className='relative'>
 			<div className='flex flex-row items-center gap-3'>
 				<div
@@ -10,6 +12,12 @@ const UserMenu = (props: Props) => {
 					className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'
 				>
 					AirBnB your home
+				</div>
+				<div
+					className='p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'
+					onClick={() => {}}
+				>
+					<AiOutlineMenu />
 				</div>
 			</div>
 		</div>
