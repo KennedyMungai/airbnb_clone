dc6ad3bbd8944714f45302980417c517427a7f24
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-29 16:23:48 +0300

    Added some markup for the footer of the modal

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -90,6 +90,12 @@ const Modal = ({
 							</div>
 							{/* Body */}
 							<div className='relative p-6 flex-auto'>{body}</div>
+							{/* Footer */}
+							<div className='flex flex-col gap-2 p-6'>
+								<div className='flex flex-row items-center gap-4 w-full'>
+									<Button />
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
