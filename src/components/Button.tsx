64f0881b2f49a258411ffeb988c066f33ca2454a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-29 16:30:03 +0300

    Added some types to the Props type

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,14 @@
-type Props = {}
+import { MouseEvent } from 'react'
+import { IconType } from 'react-icons'
+
+type Props = {
+	label: string
+	onClick: (e: MouseEvent<HTMLButtonElement>) => void
+	disabled?: boolean
+	outline?: boolean
+	small?: boolean
+	icon?: IconType
+}
 
 const Button = (props: Props) => {
 	return <button>Sample Button Title</button>
