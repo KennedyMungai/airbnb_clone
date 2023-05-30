1a0c8aace8f0d3167a45069a6065bc8611a7cdd8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-30 11:48:36 +0300

    Added a conditionally rendered icon

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,14 @@ type Props = {
 	icon?: IconType
 }
 
-const Button = ({ label, onClick, disabled, outline, small, icon }: Props) => {
+const Button = ({
+	label,
+	onClick,
+	disabled,
+	outline,
+	small,
+	icon: Icon
+}: Props) => {
 	return (
 		<button
 			className={`relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full
@@ -23,6 +30,7 @@ const Button = ({ label, onClick, disabled, outline, small, icon }: Props) => {
 			${small ? 'border-[1px]' : 'border-2'}
 			`}
 		>
+			{Icon && <Icon size={24} className='absolute left-4 top-3' />}
 			{label}
 		</button>
 	)
