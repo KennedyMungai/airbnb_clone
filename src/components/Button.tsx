6f8ae548b3f410bed22290acf0d2debc995e5d7b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-30 11:45:15 +0300

    Added some border styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -19,6 +19,8 @@ const Button = ({ label, onClick, disabled, outline, small, icon }: Props) => {
 			${outline ? 'text-black' : 'text-white'}
 			${small ? 'py-1' : 'py-3'}
 			${small ? 'text-sm' : 'text-md'}
+			${small ? 'font-light' : 'font-semibold'}
+			${small ? 'border-[1px]' : 'border-2'}
 			`}
 		>
 			{label}
