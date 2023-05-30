ca9fad7a781317b12a9f731028d1cfc802d1dd0d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-30 11:39:38 +0300

    Added some styling for the button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,15 @@ type Props = {
 }
 
 const Button = ({ label, onClick, disabled, outline, small, icon }: Props) => {
-	return <button>{label}</button>
+	return (
+		<button
+			className={`relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full ${
+				outline ? 'bh-white' : 'bg-rose-500'
+			}`}
+		>
+			{label}
+		</button>
+	)
 }
 
 export default Button
