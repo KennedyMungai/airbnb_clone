3dec74019bd754b5111a36faa36624ef87ff5136
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-30 12:00:23 +0300

    Added another button

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -13,7 +13,7 @@ type Props = {
 	actionLabel: string
 	disabled?: boolean
 	secondaryAction?: () => void
-	secondaryLabel?: string
+	secondaryActionLabel?: string
 }
 
 const Modal = ({
@@ -26,7 +26,7 @@ const Modal = ({
 	actionLabel,
 	disabled,
 	secondaryAction,
-	secondaryLabel
+	secondaryActionLabel
 }: Props) => {
 	const [showModal, setShowModal] = useState(isOpen)
 
@@ -59,14 +59,14 @@ const Modal = ({
 			return
 		}
 
-        secondaryAction()
+		secondaryAction()
 	}, [disabled, secondaryAction])
 
-    if (!isOpen) {
+	if (!isOpen) {
 		return null
-	} 
-    
-    return (
+	}
+
+	return (
 		<>
 			<div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70'>
 				<div className='relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto'>
@@ -94,6 +94,11 @@ const Modal = ({
 							{/* Footer */}
 							<div className='flex flex-col gap-2 p-6'>
 								<div className='flex flex-row items-center gap-4 w-full'>
+									<Button
+										disabled={disabled}
+										onClick={handleSubmit}
+										label={secondaryActionLabel}
+									/>
 									<Button
 										disabled={disabled}
 										onClick={handleSubmit}
