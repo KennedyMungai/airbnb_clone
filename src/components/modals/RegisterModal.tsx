d0669efb0b80ca1c3d4970a366e265c387b5b953
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-30 14:24:08 +0300

    Addd the yet to be created Input component

--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -43,6 +43,7 @@ const RegisterModal = (props: Props) => {
     const bodyContent = (
 		<div className='flex flex-col gap-4'>
 			<Heading title='Welcome to AirBnB' subtitle='Create An Account' />
+			<Input />
 		</div>
 	)
 
