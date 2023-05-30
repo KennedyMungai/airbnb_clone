11923b4097c90430c984acf67b884c7ddc84176c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-30 14:34:10 +0300

    Added an input html tag and added props to it

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form'
+import { BiDollar } from 'react-icons/bi'
 
 type Props = {
 	id: string
@@ -22,7 +23,22 @@ const Input = ({
 	register,
 	errors
 }: Props) => {
-	return <div>Input</div>
+	return (
+		<div className='w-full relative'>
+			{formatPrice && (
+				<BiDollar
+					size={'24'}
+					className='text-neutral-700 absolute top-5 left-2'
+				/>
+			)}
+			<input
+				id={id}
+				disabled={disabled}
+				{...register(id, { required })}
+				placeholder=' '
+			/>
+		</div>
+	)
 }
 
 export default Input
