191bab815426c22f72e29e83999be5608a753852
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-30 11:42:34 +0300

    Added some conditional styles for the button type

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,10 +13,11 @@ type Props = {
 const Button = ({ label, onClick, disabled, outline, small, icon }: Props) => {
 	return (
 		<button
-			className={`relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full p-2
+			className={`relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full
 			${outline ? 'bg-white' : 'bg-rose-500'}
 			${outline ? 'border-black' : 'border-rose-500'}
-			${outline ? 'text-black' : 'text-white'}`}
+			${outline ? 'text-black' : 'text-white'}
+			${small ? 'py-1' : 'py-3'}`}
 		>
 			{label}
 		</button>
